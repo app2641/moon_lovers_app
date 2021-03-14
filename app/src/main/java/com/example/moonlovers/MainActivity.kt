bab9d6b300e4814f67c21ab1ee42891096cee082
app2641 <app2641@gmail.com>
2021-03-14 18:54:25 +0900

    Base activity

--- app/src/main/java/com/example/moonlovers/MainActivity.kt
+++ app/src/main/java/com/example/moonlovers/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.moonlovers
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.moonlovers.databinding.ActivityMainBinding
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setTodayText()
+    }
+
+    private fun setTodayText() {
+        val current = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日")
+        val formattedText = current.format(formatter)
+
+        binding.today.text = formattedText
     }
 }
