a0a443d5fcd3615ef72c1aa1755c6b05e98365ee
app2641 <app2641@gmail.com>
2021-03-15 21:55:53 +0900

    Use Toolbar

--- app/src/main/java/com/example/moonlovers/MainActivity.kt
+++ app/src/main/java/com/example/moonlovers/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        setSupportActionBar(binding.mainToolbar)
 
         setTodayText()
     }
