ea1f2438d00dee50f04d7e446641872abeeb850c
app2641 <app2641@gmail.com>
2021-03-31 22:16:30 +0900

    Prepare preferences of managing class

---
+++ app/src/main/java/com/example/moonlovers/preferences/MoonLoversPreference.kt
@@ -0,0 +1,19 @@
+package com.example.moonlovers.preferences
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
+
+class MoonLoversPreference(context: Context) {
+    private val lastFetchedAtKey = "last_fetched_at"
+    private val moonAgeKey = "moon_age"
+    private val preference: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+    fun getLastFetchedAt(): String {
+        preference.getString(lastFetchedAtKey, "2021/01/01 00:00").toString()
+    }
+
+    fun getMoonAge(): String {
+        preference.getString(moonAgeKey, "15.0").toString()
+    }
+}
