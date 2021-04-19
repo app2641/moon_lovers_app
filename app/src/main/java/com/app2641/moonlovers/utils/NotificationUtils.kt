e2b3ff7667c8b4b6c15b7f4eb086e8d81f5a152f
app2641 <app2641@gmail.com>
2021-04-20 00:54:07 +0900

    Build notification

--- app/src/main/java/com/app2641/moonlovers/utils/NotificationUtils.kt
+++ app/src/main/java/com/app2641/moonlovers/utils/NotificationUtils.kt
@@ -1,8 +1,39 @@
 package com.app2641.moonlovers.utils
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import androidx.core.app.NotificationCompat
+import com.app2641.moonlovers.MainActivity
+import com.app2641.moonlovers.R
 
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
 
+    val contentPendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0,
+            contentIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    val icon = BitmapFactory.decodeResource(
+            applicationContext.resources,
+            R.drawable.img_moon15
+    )
+
+    val builder = NotificationCompat.Builder(
+            applicationContext,
+            applicationContext.getString(R.string.fcm_channel_id)
+    )
+            .setSmallIcon(R.drawable.img_moon15)
+            .setContentTitle(applicationContext.getString(R.string.fcm_channel_name))
+            .setContentIntent(contentPendingIntent)
+            .setLargeIcon(icon)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setAutoCancel(true)
+
+    notify(0, builder.build())
 }
