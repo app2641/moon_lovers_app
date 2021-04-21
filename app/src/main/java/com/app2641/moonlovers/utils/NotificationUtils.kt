14ff0ae813faefe6b6650195934452668d8ca1c7
app2641 <app2641@gmail.com>
2021-04-22 03:15:25 +0900

    add Notification icon

--- app/src/main/java/com/app2641/moonlovers/utils/NotificationUtils.kt
+++ app/src/main/java/com/app2641/moonlovers/utils/NotificationUtils.kt
@@ -1,5 +1,6 @@
 package com.app2641.moonlovers.utils
 
+import android.annotation.SuppressLint
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
@@ -8,6 +9,7 @@ import androidx.core.app.NotificationCompat
 import com.app2641.moonlovers.MainActivity
 import com.app2641.moonlovers.R
 
+@SuppressLint("ResourceAsColor")
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
     val contentIntent = Intent(applicationContext, MainActivity::class.java)
 
@@ -22,7 +24,8 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
             applicationContext,
             applicationContext.getString(R.string.fcm_channel_id)
     )
-            .setSmallIcon(R.drawable.img_moon15)
+            .setSmallIcon(R.drawable.ic_moon_lovers_notification)
+            .setColor(R.color.grey_500)
             .setContentTitle(applicationContext.getString(R.string.fcm_channel_name))
             .setContentIntent(contentPendingIntent)
             .setLargeIcon(ResourceUtils.getBitmap(applicationContext, R.drawable.img_moon15))
