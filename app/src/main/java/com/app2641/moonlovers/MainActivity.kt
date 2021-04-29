2155dbcc77e6214fbf54d6c14020a8a922765183
app2641 <app2641@gmail.com>
2021-04-29 12:59:28 +0900

    Add overflow menu

--- app/src/main/java/com/app2641/moonlovers/MainActivity.kt
+++ app/src/main/java/com/app2641/moonlovers/MainActivity.kt
@@ -3,9 +3,13 @@ package com.app2641.moonlovers
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.OnRebindCallback
@@ -56,22 +60,40 @@ class MainActivity : AppCompatActivity() {
         setTodayText()
     }
 
-    private fun setTodayText() {
-        binding?.let {
-            val current = LocalDateTime.now()
-            val formatter =  DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm")
-            val formattedText = current.format(formatter)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
 
-            binding.today.text = formattedText
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_about_notification -> {
+                val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                        .putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+                startActivity(intent)
+                true
+            }
+            R.id.menu_about_app -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun setTodayText() {
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm")
+        val formattedText = current.format(formatter)
+
+        binding.today.text = formattedText
     }
 
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel(
-                getString(R.string.fcm_channel_id),
-                getString(R.string.fcm_channel_name),
-                NotificationManager.IMPORTANCE_LOW
+                    getString(R.string.fcm_channel_id),
+                    getString(R.string.fcm_channel_name),
+                    NotificationManager.IMPORTANCE_LOW
             ).apply {
                 setShowBadge(false)
                 description = getString(R.string.fcm_channel_description)
