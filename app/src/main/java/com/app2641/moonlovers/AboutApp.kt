9bd9e0dfcef70db0047643eed203618c191f967d
app2641 <app2641@gmail.com>
2021-05-12 01:40:20 +0900

    Add author and contant

--- app/src/main/java/com/app2641/moonlovers/AboutApp.kt
+++ app/src/main/java/com/app2641/moonlovers/AboutApp.kt
@@ -1,6 +1,10 @@
 package com.app2641.moonlovers
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class AboutApp : AppCompatActivity() {
@@ -13,5 +17,14 @@ class AboutApp : AppCompatActivity() {
         toolbar.title = getString(R.string.about_app_title)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val twitter: ImageView = findViewById(R.id.twitter_icon)
+        twitter.setOnClickListener(View.OnClickListener {
+            val intent = Intent()
+            intent.setAction(Intent.ACTION_VIEW)
+            intent.addCategory(Intent.CATEGORY_BROWSABLE)
+            intent.setData(Uri.parse("https://twitter.com/app2641"))
+            startActivity(intent)
+        })
     }
 }
